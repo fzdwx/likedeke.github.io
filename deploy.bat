2fff507ff90332d69c514d3919efa3e2bd8f2196
like <980650920@qq.com>
2021-09-23 12:23:20 +0800

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-cmd /k "cd website && npm run build && cd ../docs && git add -A && git commit -m 'deploy' && git push"
+cmd /k "rmdir /s/q docs && cd website && npm run build && cd ../docs && git add -A && git commit -m 'deploy' && git push"
